638e23e441e4f5cc6d8a66485e7d9ea5bc7439f5
lhbecalli <luizhenriquebecallidefaria@gmail.com>
2025-12-03 11:46:57 -0300

    A grid was createad, more players and descriptions were added, and the creation of the first section began

---
+++ index.js
@@ -0,0 +1,18 @@
+// 1 . Pega o elemente do carrosel pelo ID 
+const myCarousel =  document.getElementById('meuCarrossel')
+
+//2. Adiciona um ouvinte a constante "Um evento" que dispara quando o slide mudar
+myCarousel.addEventListener('slide.bs.carousel', event => {
+
+    // 'event.relatedTarget' é o slide (div) que está entrando (o novo)
+const proximoSlide = event.relatedTarget;
+
+// 3. Pega o conteúdo dos atributos 'data-' do slide que vai entrar
+const novoTitulo = proximoSlide.getAttribute('data-titulo');
+const novaDescricao = proximoSlide.getAttribute('data-desc');
+
+document.getElementById('display-titulo').innerText = novoTitulo;
+document.getElementById('display-descricao').innerText = novaDescricao;
+
+
+});
